04cc97bf6071f1bcad5bb817329249f0da6f776d
Md. Mahbub Alam <mmahbub569@gmail.com>
2024-12-08 18:28:05 +0600

    updated frontend code

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://localhost:7284/api',
+  stripe: {
+    publicKey: 'pk_test_51QSH8KJJodbbdv8e45vr4FDkM7b2PR0IOehuzYGhvq7ts6cnlwPvfSg3807fYAoChBYaOIZXr7o422Oeix2gsr2G00kncl13tR',
+  }
 };
